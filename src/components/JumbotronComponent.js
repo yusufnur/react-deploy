fb14076f2eb900c01fad1d5a80f24d60213ed385
yusuf nur faizal <yusufnurfaizal@gmail.com>
2020-05-22 23:41:52 +0700

    keempat

--- src/components/JumbotronComponent.js
+++ src/components/JumbotronComponent.js
@@ -6,15 +6,13 @@ const JumbotronComponent = (props) => {
     <div>
       <Jumbotron>
         <Container>
-          <h1 className="display-3">Pendataan warga</h1>
+          <h1 className="display-3">Siaga COVID-19</h1>
           <p className="lead">
-            This is a simple hero unit, a simple Jumbotron-style component for
-            calling extra attention to featured content or information.
+            Pemerintah Desa Ngasinan Kecamatan Jetis Kabupaten Ponorogo
           </p>
           <hr className="my-2" />
           <p>
-            It uses utility classes for typography and spacing to space content
-            out within the larger container.
+            Program pendataan warga desa dari luar daerah, dalam penanganan COVID-19
           </p>
         </Container>
       </Jumbotron>
