3c6c2e5749dfc01e14f1aca6eacae739c38a32c7
yusuf nur faizal <yusufnurfaizal@gmail.com>
2020-05-22 23:22:32 +0700

    Updates

---
+++ precache-manifest.9c976d3542397ec0a5e25595b1625598.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6bb926898f4cc99e406b3b75785dc2a2",
+    "url": "/react-deploy/index.html"
+  },
+  {
+    "revision": "07cec969b1db518d0447",
+    "url": "/react-deploy/static/css/2.7ef9b196.chunk.css"
+  },
+  {
+    "revision": "f7be472e0ce24f4d996c",
+    "url": "/react-deploy/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "07cec969b1db518d0447",
+    "url": "/react-deploy/static/js/2.c181c8df.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/react-deploy/static/js/2.c181c8df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f7be472e0ce24f4d996c",
+    "url": "/react-deploy/static/js/main.4f74a529.chunk.js"
+  },
+  {
+    "revision": "8e4b5311617d720aa31b",
+    "url": "/react-deploy/static/js/runtime-main.ccd09c09.js"
+  }
+]);
